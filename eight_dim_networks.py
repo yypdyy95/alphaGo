5bc3ad64197d0fb105e5ee288ba087a9912ba350
yypdyy95 <philip.dietrich@stud.uni-goettingen.de>
2017-03-13 13:58:18 +0100

    added comments in eight_dim_networks.py

--- eight_dim_networks.py
+++ eight_dim_networks.py
@@ -8,9 +8,8 @@ from matplotlib.colors import LinearSegmentedColormap
 import time
 
 ##### Loading Data #############################################################
-# reading all files seperatly
-
-
+# either read all files seperatly, to use single years for training/ validation
+# or simply use whole dataset or testset containing games of all years together
 data2001 = './data/KGS-2001-19-2298-train10k.dat'
 data2002 = './data/KGS-2002-19-3646-train10k.dat'
 data2003 = './data/KGS-2003-19-7582-train10k.dat'
@@ -21,6 +20,7 @@ data2007 = './data/KGS-2007-19-11644-train10k.dat'
 
 testset = open('./data/kgsgo-test.dat', "rb")
 dataset = open('./data/kgsgo-train10k.dat', "rb")
+
 '''
 goboard1 = open(data2001, "rb")
 goboard2 = open(data2002, "rb")
@@ -48,37 +48,36 @@ goboard6.close()
 goboard7.close()
 '''
 # transform data to numpy array
-#pos = np.concatenate((pos1[:len(pos1)-3],pos3[:len(pos3)-3],pos4[:len(pos4)-3],pos5[:len(pos5)-3], pos6[:len(pos6)-3], pos7[:len(pos7)-3], pos2))
+# for test data change filename here
 pos = np.array(list(dataset.read()))
-#pos = np.array(pos4)
+
 
 # single moves are stored as: 2 bytes GO, 2 bytes next move, 19*19 = 361 Bytes Board
 bytes_per_move = 365
+# there are actually 3 more elements in pos array ('E' 'N' 'D' at the end of a file)
+# due to rounding the following gives correct number of moves
 number_of_moves = int(pos.shape[0]/bytes_per_move)
 
-# array of all moves from file:
+print ("loaded %d board positions" % number_of_moves )
+# array of all moves from file as 1D arrays:
 go_game = np.zeros((number_of_moves, bytes_per_move))
 
 for move in np.arange(number_of_moves):
     go_game[move] = pos[(move*bytes_per_move):((move+1)*bytes_per_move)]
 
-# get coordinates of next move
+
+# get coordinates of next move (stored in two bytes in front of every new board position)
 next_move = (go_game[:, 2:4]).astype(int)
 
 go_game = go_game[:, 4:] #discard first 4 entries with GO, label for next move
 
+# properties of every field on go board are stored in single bits -> acces bits via unpackbits
 go_game_bits = np.unpackbits(go_game.astype(np.uint8), axis=1)
-
-print(go_game_bits[0:50])
+# reshpaing training data as 19*19 array with 8 entries each -> needed to apply Convolution
 td = go_game_bits.copy()
 training_data_full = np.reshape(td.flatten(), (number_of_moves, 19 ,19, 8 ))
 
-print ("loaded %d board positions" % number_of_moves )
 
-number_of_training_positions = int(0.9*number_of_moves)
-
-training_data = training_data_full[:number_of_training_positions]
-validation_data = training_data_full[number_of_training_positions:]
 target_vectors = np.zeros((number_of_moves, 19 * 19))
 
 target_vectors[np.arange(number_of_moves), 19 * next_move[:,0] + next_move[:,1]] = 1
@@ -88,7 +87,7 @@ labels = np.reshape(target_vectors, (number_of_moves, 19 * 19))
 #########################################################################################
 # Training:
 
-# which model should be used?
+# which model should be used? (all models are pretrained)
 # options: 'CRS_fullinfo.h5' -> Convolutional(16 filter, 5*5), ReLu, softmax
 '''
 model.add(Convolution2D(4,5,border_mode = 'same', input_shape = (8,19,19)))
@@ -97,36 +96,24 @@ model.add(Dense(19*19, init = 'uniform', activation = 'softmax'))
 model.add(Reshape((19,19)))
 '''
 #          'clark_et_all_fullinfo_network.h5' -> 4* Convolutional, 1 softmax (note: use full data for training)
-#          'C1RS_fullinfo.h5' -> same as CRS, but 1 Filter instead of 4
-used_model = './networks/C32S_fullinfo.h5'# './networks/clark_et_all_fullinfo_network.h5'
+#          'C32RS_fullinfo.h5' -> same as CRS, but 1 Filter instead of 4
+
+used_model = './networks/CRS_fullinfo.h5'#'./networks/C32S_fullinfo.h5'
 
 #model = load_model(used_model)
 
 model = Sequential()
 model.add(Convolution2D(32,5,5,border_mode = 'same', input_shape = (19,19,8)))
 model.add(Flatten())
-#model.add(Dense(19*19, init = 'uniform', activation = 'relu'))
 model.add(Dense(19*19, init = 'uniform', activation = 'softmax'))
-#model.add(Reshape((19 * 19)))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
 
-'''
-model = Sequential()
-model.add(Convolution2D(48,7,7,border_mode = 'same', input_shape = (19, 19, 8)))
-model.add(Convolution2D(32,5,5,border_mode = 'same'))
-model.add(Convolution2D(32,5,5,border_mode = 'same'))
-model.add(Convolution2D(32,5,5,border_mode = 'same'))
-model.add(Flatten())
-model.add(Dense(19*19, init = 'uniform', activation = 'softmax'))
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
-'''
-hist = model.fit(training_data_full,labels, nb_epoch=20, batch_size=128, validation_split = 0.09)
+hist = model.fit(training_data_full,labels, nb_epoch=1, batch_size=128, validation_split = 0.09)
 
 # store training results in same file:
 model.save(used_model)
 ########################################################################################
-#plotting training history:
+#plotting training history: (history is returned as dictionary by keras fit function)
 
 fig = plt.figure(figsize = (8,8))
 plt.xlabel("Epoch")
@@ -143,10 +130,47 @@ plt.legend(loc = 'best')
 # more evaluation:
 # check number of correctly predicted moves on full data set
 # stop time, to get time for one prediction
+
 start_time = int(round(time.time() * 1000))
 predictions = model.predict(training_data_full, verbose = 1)
 sim_time = int(round(time.time() * 1000))-start_time
 print("takes ", (sim_time/number_of_moves), "milliseconds to predict move" )
+predictions.reshape(number_of_moves, 19,19)
+
+acc = 0
+for i in range(number_of_moves):
+    # field with highest accuracy in result -> predicted move
+    predicted_move = np.argmax(predictions[i])
+    predicted_move = [int(predicted_move/19), predicted_move % 19]
+    #print ("real move: ", next_move[i], "  predicted move: ", predicted_move)
+    if next_move[i][0] == predicted_move[0] and predicted_move[1] == next_move[i][1]:
+        acc +=1
+
+print ( acc/number_of_moves*100 , "percent of moves predicted correctly on training and validation set" )
+###########################################################################################
+# same steps as beforr for testset to get accuracy on testset
+
+test = np.array(list(testset.read()))
+number_of_moves = int(test.shape[0]/bytes_per_move)
+
+# array of all moves from file as 1D arrays:
+go_game = np.zeros((number_of_moves, bytes_per_move))
+
+for move in np.arange(number_of_moves):
+    go_game[move] = test[(move*bytes_per_move):((move+1)*bytes_per_move)]
+
+# get coordinates of next move (stored in two bytes in front of every new board position)
+next_move = (go_game[:, 2:4]).astype(int)
+
+go_game = go_game[:, 4:] #discard first 4 entries with GO, label for next move
+
+# properties of every field on go board are stored in single bits -> acces bits via unpackbits
+go_game_bits = np.unpackbits(go_game.astype(np.uint8), axis=1)
+# reshpaing training data as 19*19 array with 8 entries each -> needed to apply Convolution
+testdata = go_game_bits.copy()
+training_data_full = np.reshape(testdata.flatten(), (number_of_moves, 19 ,19, 8 ))
+
+predictions = model.predict(training_data_full, verbose = 1)
 
 predictions.reshape(number_of_moves, 19,19)
 
@@ -159,5 +183,6 @@ for i in range(number_of_moves):
     if next_move[i][0] == predicted_move[0] and predicted_move[1] == next_move[i][1]:
         acc +=1
 
-print ( acc/number_of_moves*100 , "percent of moves predicted correctly" )
+print ( acc/number_of_moves*100 , "percent of moves predicted correctly on training and validation set" )
+
 plt.show()
