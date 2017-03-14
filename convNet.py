6d1a007148564cdaf79de55c583a30128c535425
Laurenz Hemmen <laurenz.hemmen@stud.uni-goettingen.de>
2017-03-14 11:13:41 +0100

    Add Test Mode

--- convNet.py
+++ convNet.py
@@ -16,6 +16,7 @@ number_of_moves, next_move, bit_board = util.load_data("./data/KGS-2004-19-12106
 
 
 ##### Generate Training Data ###################################################
+
 number_of_planes = 3
 x_train = np.zeros((number_of_moves, number_of_planes, 19, 19))
 for move in np.arange(number_of_moves):
@@ -28,11 +29,14 @@ print(x_train.shape)
 del bit_board
 del next_move
 gc.collect()
+
+
 ##### Train Network ############################################################
+
 model_name = './networks/CNN_pos_100k.h5'
 
 model = Sequential()
-model.add(Convolution2D(32, 5,5, input_shape=(3, 19, 19), border_mode='same', activation='relu'))
+model.add(Convolution2D(32, 3,3, input_shape=(3, 19, 19), border_mode='same', activation='relu'))
 print(model.output_shape)
 model.add(Flatten())
 print(model.output_shape)
@@ -54,14 +58,19 @@ print(history.history['val_loss'])
 
 
 ##### Test Network #############################################################
+"""
+model_name = './networks/CNN_pos_32f3.h5'
 model = load_model(model_name)
 number_of_moves, next_move, bit_board = util.load_data("./data/kgsgo-test.dat")
 gc.collect()
 
+number_of_planes = 3
 x_test = np.zeros((number_of_moves, number_of_planes, 19, 19))
 for move in np.arange(number_of_moves):
     x_test[move] = util.get_board(bit_board[move])
 
+y_test = np.zeros((number_of_moves, 19*19))
+y_test[np.arange(number_of_moves), 19*next_move[:,0]+next_move[:,1]] =1
 
 tic = time.time()
 y_pred = model.predict(x_test , batch_size=32, verbose=0)
@@ -75,3 +84,4 @@ how_likely=np.amax(y_pred, axis=1)
 # accuracy: How much percent of moves is correctly predicted
 acc_test = np.mean(which_move == np.argmax(y_test, axis=1))
 print('test accuracy: {0}'.format(acc_test))
+"""
