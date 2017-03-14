82ea10db8116521e28df4715571ce3cd4ade4305
Laurenz Hemmen <laurenz.hemmen@stud.uni-goettingen.de>
2017-03-14 10:06:35 +0100

    Add testing for ConvNet

--- convNet.py
+++ convNet.py
@@ -11,8 +11,8 @@ K.set_image_dim_ordering('th')
 
 
 ##### Load Data ################################################################
-#number_of_moves, next_move, bit_board = util.load_data("./data/KGS-2004-19-12106-train10k.dat", max_moves=100000)
-number_of_moves, next_move, bit_board = util.load_data("./data/kgsgo-train10k.dat")
+number_of_moves, next_move, bit_board = util.load_data("./data/KGS-2004-19-12106-train10k.dat", max_moves=500000)
+#number_of_moves, next_move, bit_board = util.load_data("./data/kgsgo-train10k.dat")
 
 
 ##### Generate Training Data ###################################################
@@ -32,13 +32,46 @@ gc.collect()
 model_name = './networks/CNN_pos_100k.h5'
 
 model = Sequential()
-model.add(Convolution2D(32, 3,3, input_shape=(3, 19, 19), border_mode='same', activation='relu'))
+model.add(Convolution2D(32, 5,5, input_shape=(3, 19, 19), border_mode='same', activation='relu'))
 print(model.output_shape)
 model.add(Flatten())
 print(model.output_shape)
 model.add(Dense(361, init = 'uniform', activation = 'softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-hist = model.fit(x_train, y_train, nb_epoch=10, batch_size=4096, validation_split = 0.05)
+history = model.fit(x_train, y_train, nb_epoch=10, batch_size=4096, validation_split = 0.05)
 
 model.save(model_name)
+
+print("Categorical Accuracy:")
+print(history.history['categorical_accuracy'])
+print("Validation Categorical Accuracy:")
+print(history.history['val_categorical_accuracy'])
+print("Loss:")
+print(history.history['loss'])
+print("Validation Loss:")
+print(history.history['val_loss'])
+
+
+##### Test Network #############################################################
+model = load_model(model_name)
+number_of_moves, next_move, bit_board = util.load_data("./data/kgsgo-test.dat")
+gc.collect()
+
+x_test = np.zeros((number_of_moves, number_of_planes, 19, 19))
+for move in np.arange(number_of_moves):
+    x_test[move] = util.get_board(bit_board[move])
+
+
+tic = time.time()
+y_pred = model.predict(x_test , batch_size=32, verbose=0)
+toc = time.time()
+print("prediction time = {}".format(toc-tic))
+# coordinates of most likely move:
+which_move=np.argmax(y_pred, axis=1)
+# "probability" of this move:
+how_likely=np.amax(y_pred, axis=1)
+
+# accuracy: How much percent of moves is correctly predicted
+acc_test = np.mean(which_move == np.argmax(y_test, axis=1))
+print('test accuracy: {0}'.format(acc_test))
