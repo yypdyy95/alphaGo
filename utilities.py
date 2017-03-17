a5878fc29b5ed3346dac843e94051b56737ba121
yypdyy95 <philip.dietrich@stud.uni-goettingen.de>
2017-03-17 15:36:26 +0100

    added Filter visualisation

--- utilities.py
+++ utilities.py
@@ -529,6 +529,47 @@ def plot_loss_acc(histories):
 
     plt.show()
 
+def visualize_weights(layer, max_filters = 100):
+    '''
+    plotting weight matrices of convolutional layer
+
+    Inputs:
+    - layer: Conv layer of which weights should be visualized
+    '''
+    weights = layer.get_weights()
+    weights = np.array(weights[0])
+
+    number_of_filters = len(weights)
+    if max_filters < number_of_filters:
+        number_of_filters = max_filters
+    number_of_planes = len(weights[0])
+
+    fig, ax = plt.subplots( number_of_filters,number_of_planes)
+    fig.set_size_inches(11,9)
+    fig.suptitle('Visualisation of Filters', fontsize=20)
+    left  = 0.05  # the left side of the subplots of the figure
+    right = 0.98    # the right side of the subplots of the figure
+    bottom = 0.02   # the bottom of the subplots of the figure
+    top = 0.85      # the top of the subplots of the figure
+    wspace = 0.05   # the amount of width reserved for blank space between subplots
+    hspace = 0.05   # the amount of height reserved for white space between subplots
+    plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+    print(ax.shape)
+    for i in range(number_of_planes):
+        ax[0,i].set_title("plane %d" %(i+1))
+        for j in range(number_of_filters):
+            if i == 0:
+                ax[j,i].text(left,0.5*(bottom+top), 'filter %d' %(j+1),
+                horizontalalignment='right',
+                verticalalignment='center',
+                rotation='vertical',
+                transform=ax[j,i].transAxes)
+
+            ax[j,i].axis('off')
+            im = ax[j,i].imshow(weights[j,i])
+    fig.colorbar(im, ax=ax.ravel().tolist())
+    plt.show()
+
 
 
 
